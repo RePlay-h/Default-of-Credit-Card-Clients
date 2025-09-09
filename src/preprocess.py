Create test.py to calculate metrics (recall, precision, f1, auc) from test samples Delete cat_features list for CatBoost Fix training problems with baseline and CatBoost Create pipeline with the help of dvc Change some lines into params.yaml

src/preprocess.py
@@ -3,6 +3,9 @@ import numpy as np
 
 import yaml
 
+import warnings
+
+warnings.filterwarnings("ignore")
 
 def preprocess(data_path):
     
@@ -30,7 +33,11 @@ def preprocess(data_path):
     # if payment_ratio < 1 - a client pays less than his debt
     bill_cols = ["BILL_AMT1", "BILL_AMT2", "BILL_AMT3", "BILL_AMT4", "BILL_AMT5", "BILL_AMT6"]
     pay_amt_cols  = ["PAY_AMT1", "PAY_AMT2", "PAY_AMT3", "PAY_AMT4", "PAY_AMT5", "PAY_AMT6"]
-    df['payment_ratio'] = df[pay_amt_cols].sum(axis=1) / df[bill_cols].sum(axis=1)
+    df['payment_ratio'] = np.where(
+        df[bill_cols].sum(axis=1) != 0,
+        df[pay_amt_cols].sum(axis=1) / df[bill_cols].sum(axis=1),
+        1
+    )
 
     return df
 
@@ -40,7 +47,7 @@ if __name__ == '__main__':
     
     df = preprocess(params['input'])
 
-    df.to_csv(params['output'])
+    df.to_csv(params['output'], index=False)
 
     print(f'Processed dataset: {params['output']}')
 

src/train.py
@@ -8,17 +8,22 @@ import os
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import RandomizedSearchCV 
 from sklearn.model_selection import train_test_split, cross_val_score
-from scipy.stats import loguniform
+from scipy.stats import loguniform, uniform
 from catboost import CatBoostClassifier
 import optuna
+
+import warnings
+
+warnings.filterwarnings("ignore")
 
 def train_baseline(X_train, y_train, model_path):
    
     # Train baseline model
-    lr = LogisticRegression(max_iter=300)
+    lr = LogisticRegression(max_iter=300, solver='saga')
     distributions = {
         'C': loguniform(1e-2, 1e2),
-        'penalty': ['l1', 'l2', 'elasticnet']
+        'penalty': ['elasticnet', 'l1', 'l2'],
+        'l1_ratio': uniform(0, 1)
     }
 
     # Tuning baseline model
@@ -26,15 +31,17 @@ def train_baseline(X_train, y_train, model_path):
         estimator=lr,
         param_distributions=distributions,
         n_iter=30,
-        scoring='auc-roc',
+        scoring='roc_auc',
         cv=7,
-        random_state=101
+        random_state=101,
+        verbose=0
     )
 
+    print("Start training and tuning baseline")
     random_search.fit(X_train, y_train)
 
     print(f'Best params (LogisticRegression): {random_search.best_params_}')
-    print(f'Best score (LogisticRegression): {random_search.best_score_}')
+    print(f'Best AUC (LogisticRegression): {random_search.best_score_}')
 
     os.makedirs(os.path.dirname(model_path), exist_ok=True)
     pickle.dump(random_search, open(model_path, 'wb'))
@@ -47,10 +54,10 @@ def objective(trial):
     # define hyperparams for optuna
     params = {
         'iterations': trial.suggest_int("iterations", 500, 1000),
-        'depth': trial.suggest_int("depth", 2, 3, 5, 7, 10),
+        'depth': trial.suggest_int("depth", 2, 10),
         'learning_rate': trial.suggest_loguniform("learning_rate", 1e-2, 0.5),
-        'l2_leaf_reg': trial.suggest_loguniform("l2_leaf_reg", 1e-2, 1e2),
-        'border_count': trial.suggest_int("border_count", 32, 128, 254),
+        'l2_leaf_reg': trial.suggest_loguniform("l2_leaf_reg", 1e-2, 0.3),
+        'border_count': trial.suggest_int("border_count", 32, 254),
         'random_strength': trial.suggest_float("random_strength", 0.0, 10.0),
         'eval_metric': "AUC",
         'verbose': 0,
@@ -70,14 +77,15 @@ def train(X_train, y_train, boosting_path):
     cat_features = [1, 2, 3]
 
     # select hyperparams
+    print("Start training and tuning CatBoost")
     study = optuna.create_study(direction='maximize', sampler=optuna.samplers.TPESampler(seed=42))
-    study.optimize(objective, n_trials=75)
+    study.optimize(objective, n_trials=10)
 
     print(f"Best params (CatBoost): {study.best_params}")
     print(f"Best AUC on train (CatBoost): {study.best_value}")
 
     best_model = CatBoostClassifier(**study.best_params)
-    best_model.fit(X_train, y_train, cat_feature=cat_features)
+    best_model.fit(X_train, y_train, cat_features=cat_features)
 
     os.makedirs(os.path.dirname(boosting_path), exist_ok=True)
     pickle.dump(best_model, open(boosting_path, 'wb'))
@@ -91,16 +99,16 @@ def train(X_train, y_train, boosting_path):
 def save_test_samples(X_test, y_test, X_test_path, y_test_path):
     os.makedirs(os.path.dirname(X_test_path), exist_ok=True)
     os.makedirs(os.path.dirname(y_test_path), exist_ok=True)
-    X_test.to_csv(X_test_path)
-    y_test.to_csv(y_test_path)
+    X_test.to_csv(X_test_path, index=False)
+    y_test.to_csv(y_test_path, index=False)
 
 if __name__ == '__main__':
     # get dataset
     params = yaml.safe_load(open("params.yaml"))['train']
-    dataset = pd.csv_read(params['input'])
+    dataset = pd.read_csv(params['input'])
 
     # One hot-encoding for EDUCATION feature
-    df_encoded = pd.get_dummies(dataset, columns=['EDUCATION'], drop_first=True, dtype=np.int64)
+    df_encoded = pd.get_dummies(dataset, columns=['EDUCATION', 'MARRIAGE'], drop_first=True, dtype=np.int64)
 
     # Create train-test samples
     X, y = df_encoded.drop('default payment next month', axis=1), df_encoded['default payment next month']